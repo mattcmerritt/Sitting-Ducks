f1f7d5da4c8e9eebfb6e418bc706e1c7c5de8992
mikepmerritt <mikepmerritt@gmail.com>
2021-03-18 04:05:04 -0400

    added prefab and script for reverse vertical tile

---
+++ Assets/Scripts/Current/ReverseVerticalTile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseVerticalTile : Tile
+{
+    public override void actOnPlayer(Player duck)
+    {
+        base.actOnPlayer(duck);
+        duck.setVelocity(duck.getCurrentVelocity().x, -duck.getCurrentVelocity().y);
+    }
+}
